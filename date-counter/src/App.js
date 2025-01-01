12c8daa4db6596ee63326579427727efb8849063
Kabir Kashif <kashif.kabir60@gmail.com>
2025-01-01 15:36:12 +0500

    completed 'date-counter' app

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -1,23 +1,47 @@
+import { use, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const todayDate = new Date(`${new Date().toDateString()}`);
+  todayDate.setDate(todayDate.getDate() + count);
+
+  const handleAddCount = () => {
+    setCount((currCount) => currCount + step);
+  };
+
+  const handleSubtractCount = () => {
+    setCount((currCount) => currCount - step);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <button onClick={() => setStep((currStep) => currStep - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((currStep) => currStep + 1)}>+</button>
+      </div>
+
+      <div>
+        <button onClick={handleSubtractCount}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handleAddCount}>+</button>
+      </div>
+
+      {count === 0 && (
+        <div>Today is {todayDate.toDateString()}</div>
+      )}
+
+      {count < 0 && (
+        <div>{Math.abs(count)} days ago was {todayDate.toDateString()}</div>
+      )}
+
+      {count > 0 && (
+        <div>{count} days from today will be {todayDate.toDateString()}</div>
+      )}
     </div>
   );
 }
