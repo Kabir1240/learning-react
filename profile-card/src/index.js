e6c8d3f5454d396afe48ea42168a46ce0b071589
Kabir Kashif <kashif.kabir60@gmail.com>
2025-01-01 09:24:18 +0500

    created and initialized 'steps' app for section 6

--- profile-card/src/index.js
+++ profile-card/src/index.js
@@ -2,6 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
+const skills = [
+  {
+    name:"python",
+    level:"advanced",
+    color:"yellow"
+  },
+  {
+    name:"React",
+    level:"beginner",
+    color:"green"
+  },
+  {
+    name:"git",
+    level:"intermediate",
+    color:"blue"
+  },
+]
+
 function App() {
   const Avatar = () => {
     return <img className="avatar" src="./my_picture.jpg" alt="me" />;
@@ -16,11 +34,18 @@ function App() {
     )
   }
 
-  const Skill = (props) => {
-    return ( 
-        <div className="skill" style={{ backgroundColor: props.color }}>
-          <span>{props.skill}</span>
-          <span>{props.emoji}</span>
+  const Skill = ({ name, color, level }) => {
+    const getSkillEmoji = () => {
+      if (level === "advanced") return "🫡";
+      if (level === "intermediate") return "🙂";
+      if (level === "beginner") return "🌱";
+      return "❓"; // Default emoji for unknown levels
+    };
+
+    return (
+        <div className="skill" style={{ backgroundColor: color }}>
+          <span>{name}</span>
+          <span>{getSkillEmoji}</span>
         </div>
     )
   }
@@ -28,8 +53,9 @@ function App() {
   const SkillList = () => {
     return (
       <div className="skill-list">
-        <Skill color="yellow" skill="js" emoji="🫡"/>
-        <Skill color="red" skill="python" emoji="🥲"/>
+        {skills.map((skill) => (
+          <Skill color={skill.color} name={skill.name} level={skill.level}/>
+        ))}
       </div>
     );
   };
